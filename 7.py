b94fbc382b9fc447ebb20be47b4f0d4152cd4f9c
Shetty Roshith Raghurama <121103694+07Roshi@users.noreply.github.com>
2023-07-24 08:43:15 +0000

    update

--- 7.py
+++ 7.py
@@ -3,7 +3,7 @@ import networkx as nx
 def get_distances(num_nodes):
     distances = {}
     for i in range(1, num_nodes+1):
-        for j in range(1, num_nodes+1):
+        for j in range(i+1, num_nodes+1):
             distance = float(input(f"Enter the distance between node {i} and node {j}: "))
             distances[(i, j)] = distance
             distances[(j, i)] = distance
